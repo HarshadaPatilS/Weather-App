ed2c911af793099b67ac294e1db400bbcb6be3ac
Harshada Patil <harshaa.prv@gmail.com>
2025-10-06 01:00:57 +0530

    api key

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // API Configuration
-const API_KEY = 'fada407be0495e2675ed0fa58e14188d'; // Replace with your OpenWeatherMap API key
+const API_KEY = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
 const API_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
